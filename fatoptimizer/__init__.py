7446e321f087b23d4817f273adbe64868152d4e2
Victor Stinner <vstinner@redhat.com>
2016-01-15 17:15:33 +0100

    Update to the PEP 511

--- fatoptimizer/__init__.py
+++ fatoptimizer/__init__.py
@@ -1,6 +1,8 @@
 from .tools import pretty_dump, OptimizerError
 from .config import Config
 from .optimizer import ModuleOptimizer as _ModuleOptimizer
+import sys
+
 
 __version__ = '0.1'
 
@@ -8,6 +10,19 @@ __version__ = '0.1'
 def optimize(tree, filename, config):
     optimizer = _ModuleOptimizer(config, filename)
     return optimizer.optimize(tree)
+
+
+class FATOptimizer:
+    name = "fat"
+
+    def __init__(self, config):
+        self.config = config
+
+    def ast_transformer(self, tree, context):
+        filename = context.filename
+        if sys.flags.verbose and not filename.startswith('<'):
+            print("# run fatoptimizer on %s" % filename, file=sys.stderr)
+        return optimize(tree, context.filename, self.config)
 
 
 def _register():
@@ -21,9 +36,7 @@ def _register():
     if sys.flags.verbose:
         config.logger = sys.stderr
 
-    def optimizer(tree, filename):
-        if sys.flags.verbose and not filename.startswith('<'):
-            print("# run fatoptimizer on %s" % filename, file=sys.stderr)
-        return optimize(tree, filename, config)
-
-    sys.set_ast_transformers([(optimizer, 'fat')])
+    transformers = sys.get_code_transformers()
+    # add the FAT optimizer before the peephole optimizer
+    transformers.insert(0, FATOptimizer(config))
+    sys.set_code_transformers(transformers)
