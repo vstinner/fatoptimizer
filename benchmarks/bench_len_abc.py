b6963f8d688fa3d59685bcb594bf9d814dc96094
Victor Stinner <vstinner@redhat.com>
2016-01-21 13:56:29 +0100

    add bench_len_abc.py benchmark

--- benchmarks/bench_len_abc.py
+++ benchmarks/bench_len_abc.py
@@ -1,5 +1,5 @@
 """
-Microbenchmark on the "copy builtin to constant".
+Microbenchmark on the "call builtin functions [with a constant]".
 
 The benchmark doesn't use fatoptimize, but specialize explicitly the function.
 """
@@ -7,26 +7,26 @@ import fat
 import sys
 from fatoptimizer.benchmark import bench, format_dt, compared_dt
 
-def func(obj):
-    return len(obj)
+def func():
+    return len("abc")
 
 if fat.get_specialized(func):
     print("ERROR: func() was already specialized")
     sys.exit(1)
 
-def func_cst(obj):
-    return 'LEN'(obj)
+def func_cst():
+    return 3
 func_cst.__code__ = fat.replace_consts(func_cst.__code__, {'LEN': len})
 
-dt = bench('func("abc")')
-print("original bytecode (LOAD_GLOBAL dict): %s" % format_dt(dt))
+dt = bench('func()')
+print("original bytecode (call len): %s" % format_dt(dt))
 
-dt2 = bench('func_cst("abc")')
-print("LOAD_CONST: %s" % compared_dt(dt2, dt))
+dt2 = bench('func_cst()')
+print("return 3: %s" % compared_dt(dt2, dt))
 
 fat.specialize(func, func_cst, [fat.GuardBuiltins(('dict',))])
 assert fat.get_specialized(func)
-dt3 = bench('func("abc")')
-print("specialized (LOAD_CONST): %s" % compared_dt(dt3, dt))
+dt3 = bench('func()')
+print("specialized (return 3): %s" % compared_dt(dt3, dt))
 
 print("cost of GuardBuiltins: %s" % format_dt(dt3 - dt2, sign=True))
