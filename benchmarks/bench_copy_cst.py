25ab9f48268c313be87145b28f8ace1b28ebe8e1
Victor Stinner <vstinner@redhat.com>
2016-01-21 14:20:16 +0100

    rewrite bench_copy_cst with my benchmark library

--- benchmarks/bench_copy_cst.py
+++ benchmarks/bench_copy_cst.py
@@ -5,28 +5,33 @@ The benchmark doesn't use fatoptimize, but specialize explicitly the function.
 """
 import fat
 import sys
-from fatoptimizer.benchmark import bench, format_dt, compared_dt
+
+mylen = len
 
 def func(obj):
-    return len(obj)
+    return mylen(obj)
 
 if fat.get_specialized(func):
     print("ERROR: func() was already specialized")
     sys.exit(1)
 
 def func_cst(obj):
-    return 'LEN'(obj)
-func_cst.__code__ = fat.replace_consts(func_cst.__code__, {'LEN': len})
+    return 'MYLEN'(obj)
+func_cst.__code__ = fat.replace_consts(func_cst.__code__, {'MYLEN': mylen})
 
-dt = bench('func("abc")')
-print("original bytecode (LOAD_GLOBAL dict): %s" % format_dt(dt))
+def run_benchmark(bench):
+    bench.timeit(stmt='func("abc")',
+                 globals=globals(),
+                 name='original bytecode (LOAD_GLOBAL)')
 
-dt2 = bench('func_cst("abc")')
-print("LOAD_CONST: %s" % compared_dt(dt2, dt))
+    bench.timeit(stmt='func_cst("abc")',
+                 globals=globals(),
+                 name='LOAD_CONST')
 
-fat.specialize(func, func_cst, [fat.GuardBuiltins(('dict',))])
-assert fat.get_specialized(func)
-dt3 = bench('func("abc")')
-print("specialized (LOAD_CONST): %s" % compared_dt(dt3, dt))
+    fat.specialize(func, func_cst, [fat.GuardGlobals(('mylen',))])
+    assert fat.get_specialized(func)
 
-print("cost of GuardBuiltins: %s" % format_dt(dt3 - dt2, sign=True))
+    bench.timeit(stmt='func("abc")',
+                 globals=globals(),
+                 name='LOAD_CONST with guard')
+
